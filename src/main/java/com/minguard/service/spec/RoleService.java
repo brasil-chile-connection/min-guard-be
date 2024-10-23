ae7c4b37468dc559109fabd024aaaacc3d127cb5
Roberto Mahl <robertomahl@live.com>
2024-10-22 23:51:40 -0300

    Add roles retrieval

--- src/main/java/com/minguard/service/spec/RoleService.java
+++ src/main/java/com/minguard/service/spec/RoleService.java
@@ -1,10 +1,14 @@
 package com.minguard.service.spec;
 
+import com.minguard.dto.role.RoleResponse;
 import com.minguard.entity.Role;
 import com.minguard.util.Roles;
+import java.util.List;
 
 public interface RoleService {
 
     Role getByName(Roles role);
+
+    List<RoleResponse> findAll();
 
 }
